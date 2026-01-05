eb612601852614bfd5fef3c855d0531669d4e090
root <george.george@oracle.com>
2026-01-05 13:02:11 +0200

    Added services/vpc_\&_network/vpc_\&_network.tf: subnet_ref0_01

--- services/vpc_&_network/vpc_&_network.tf
+++ services/vpc_&_network/vpc_&_network.tf
@@ -161,3 +161,11 @@ resource "aws_subnet" "publicsubnet00c" {
 }
 
 
+resource "aws_subnet" "subnet_ref0_01" {
+  vpc_id     = aws_vpc.vpc00.id
+  cidr_block = "10.0.254.0/24"
+
+  tags = {
+    Name = "${aws_vpc.vpc00.name}-subnet_ref0_01"
+  }
+}
